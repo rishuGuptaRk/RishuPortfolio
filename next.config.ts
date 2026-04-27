de64b9f303302f59c4b1cdcf9aa8faba6ae5b416
Rishu Gupta <rishuguptark@gmail.com>
2026-04-27 14:48:45 +0000

    Commit

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,44 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Required for GitHub Pages static deployment
   images: {
-    unoptimized: true, // Required for static export as GitHub Pages doesn't have an Image Optimization API
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
+        protocol: "https",
+        hostname: "placehold.co",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        protocol: "https",
+        hostname: "picsum.photos",
       },
       {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'c.tenor.com',
+        protocol: "https",
+        hostname: "c.tenor.com",
       },
       {
-        protocol: 'https',
-        hostname: 'thumbs.gfycat.com',
+        protocol: "https",
+        hostname: "thumbs.gfycat.com",
       },
       {
-        protocol: 'https',
-        hostname: 'pa1.narvii.com',
-      }
+        protocol: "https",
+        hostname: "pa1.narvii.com",
+      },
     ],
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
